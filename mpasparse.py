56a84377e275752665c420afc30184e96522db08
noescobar <nelson.eonltd@gmail.com>
2014-09-25 10:19:07 -0500

    correccion y modificacion del mpasparse, duda : en como se debe asiganar el valor en  caso como la asignacion de una variable

--- mpasparse.py
+++ mpasparse.py
@@ -1,90 +1,164 @@
-__author__='noescobar,rass,anagui'
-
-
-
+# coding=utf-8
+__author__ = 'noescobar,rass,anagui'
 
 import ply.yacc as yacc
 
-def p_funcion(p) :
-    '''fun : FUN funname '(' parameters ')' locals BEGIN statements END
-    | fun : FUN funname  '('')' locals  BEGIN  statements END'''
 
-def funname(p) :
-    'funname :  ID'
+def p_funcion_args(p):
+    '''
+    fun : FUN funname '(' parameters ')' locals BEGIN statements END
+    '''
+    p[0]=p[1:]
 
-def valor(p) :
-    '''valor : ID
-    |valor :int
-    |float
-    |number
-    |expression
-    |number
-     '''
+def p_funcion(p):
+    '''
+     fun : FUN funname  '('')' locals  BEGIN  statements END
+    '''
+    p[0]=p[1:]
 
-def defvar(p) :
+def funname(p):
+    '''
+    funname :  ID
+    '''
+    p[0]=p[1]
+
+def p_valor_ID(p):
+    '''
+    valor : ID
+    '''
+    p[0]=p[1]
+
+def p_valor_int(p):
+    '''
+    valor : int
+    '''
+    p[0]=p[1]
+
+def p_valor_float(p) :
+    '''
+    valor : float
+    '''
+    p[0]=p[1]
+
+def p_valor_number(p):
+    '''
+    valor : number
+    '''
+    p[0]=p[1]
+
+def p_valor_expr(p):
+    '''
+    valor :expression
+    '''
+    p[0]=p[1]
+
+
+def p_defvar_id(p):
     '''defvar :  ID ':'  tipo
-    | ID ':' tipo '[' valor ']'
-    '''
 
-def assign(p) : 
-    '''assign :  ID ASIGSIM  valor
-    | assign : ID  ASIGSIM  ID
-    | assign : ID '[' valor ']' ASIGSIM valor
-    | ID '[' valor ']' ASIGSIM ID '[' valor ']'
     '''
+    p[0]=p[1:]
+
+def p_defvar_vect(p):
+    '''
+    defvar : ID ':' tipo '[' valor ']'
+    '''
+    p[0] = p[1:]
+
+def p_parameters_multi(p):
+    '''
+    parameters : defvar ‘,’ parameters
+    '''
+    p[0] = p[1:]
+
+def p_parameters_unique(p) :
+    '''
+    parameters: defvar
+    '''
+    p[0]=p[1]
+
+def p_assign_val(p):
+    '''
+    assign :  ID ASIGSIM  valor
+    '''
+    p[0]=p[1:]
+
+def p_assign_var(p):
+    '''
+    assign : ID  ASIGSIM  ID
+    '''
+    p[0]=p[1:]
+
+def p_assign_vec(p):
+    '''
+    assign : ID '[' valor ']' ASIGSIM valor
+    '''
+    p[0]=p[1:]
+
+def p_assign_vtv(p):
+    '''
+    assign : ID '[' valor ']' ASIGSIM ID '[' valor ']'
+    '''
+    p[0]=p[1:]
+
 def p_expression_plus(p):
-    '''expression : valor "+" expression
-
     '''
-         p[0] = p[1] + p[3]
+    expression : valor "+" expression
+    '''
+    p[0] = p[1] + p[3]
+
 
 def p_expression_minus(p):
     '''
     expression : valor '-' expression
     '''
-     p[0] = p[1] - p[3]
+    p[0] = p[1] - p[3]
+
 
 def p_expression_times(p):
     '''
     expression : valor '*' expression
     '''
-     p[0] = p[1] * p[3]
+    p[0] = p[1] * p[3]
+
 
 def p_expression_divide(p):
     '''
     expression : valor '/' expression
     '''
-     p[0] = p[1] / p[3]
+    p[0] = p[1] / p[3]
+
 
 def p_expression_int(p):
     '''
     expression : INT '(' valor ')'
     '''
-     p[0] = int(p[3])
+    p[0] = int(p[3])
+
 
 def p_expression_float(p):
     '''
     expression : FLOAT '(' valor ')'
     '''
-     p[0] = float(p[3])
+    p[0] = float(p[3])
 
-    #| expression : funname '(' args ')'
-    #| expression : funname '('')'
-    #| expression : valor
+def p_expression_funargs(p):
+    '''
+    expression : funname '(' args ')'
+    '''
+    p[0]=p[1:]
+
+def p_expression_fun(p):
+    '''
+    expression : funname '('')'
+    '''
+    p[0]=p[1:]
+
+def  p_expresion_valor(p):
+    '''
+     expression : valor
+    '''
+    p[0]=p[1]
 
 
-def p_term_times(p):
-    'term : term "*" factor'
-    p[0] = p[1] * p[3]
-
-def p_term_div(p):
-    'term : term "/" factor'
-    p[0] = p[1] / p[3]
-
-def p_term_factor(p):
-    'term : factor'
-    p[0] = p[1]
-
-
-
-
+parse = yacc.yacc()
